59de9fef424d0341e0cee1da15ce7148685b50d5
Matthew Teelucksingh <matthewteelucksingh@Matthews-MacBook-Air.local>
2024-06-11 18:36:52 -0700

    Adding support for providing query params for the post extractors

--- reddit_label/src/main/java/com/reddit/label/Main.java
+++ reddit_label/src/main/java/com/reddit/label/Main.java
@@ -12,23 +12,28 @@ public class Main {
         // Command line args:
         Options options = new Options();
         options.addOption("s", "subreddit", true, "The subreddit to extract posts from");
-        options.addOption("c", "continue_past_unique", true, "If the ingestor will exit when an article pull extracts no unique posts");
-        options.addOption("ct", "create_tables", true, "If subreddit post tables in the database should try to be created");
-
+        options.addOption("c", "continue_past_unique", false, "If the ingestor will exit when an article pull extracts no unique posts");
+        options.addOption("ct", "create_tables", false, "If subreddit post tables in the database should try to be created");
+        options.addOption("ov", "override_param", true, "If the ingestor needs to start at a specific url point with query params we can use this variable");
+        
         String subreddit = "";
+        String urlOverride = null;
         boolean continuePastUnique = false;
-        boolean tryCreateTable = false;
+        boolean tryCreateTable = true;
 
         CommandLineParser parser = new DefaultParser();
         try {
             CommandLine cmd = parser.parse(options, args);
             subreddit = cmd.getOptionValue("subreddit");
             continuePastUnique = cmd.hasOption("continue_past_unique");
+            tryCreateTable = cmd.hasOption("create_table");
+            urlOverride = cmd.getOptionValue("override_param");
         } catch (ParseException e) {
             System.err.println("Error with parsing command lines" + e.getMessage());
             System.exit(1);
         }
 
+        tryCreateTable = true;
         if (tryCreateTable) {
             System.out.println("Trying to create subreddit post database tables...");
             SubredditTablesDB.createSubredditTables();
@@ -39,9 +44,13 @@ public class Main {
             subreddit,
             continuePastUnique
         ));
-
-        // SubredditPostIngestor.RunSubredditIngestor(subreddit, continuePastUnique);
-        SubredditPostIngestor.RunSubredditIngestorOld(subreddit, continuePastUnique);
+        
+        if (urlOverride != null) {
+            String.format("Manual Override provided $s. Activating Ingestor.", urlOverride);
+            SubredditPostIngestor.RunSubredditIngestorOld(subreddit, continuePastUnique, urlOverride);
+        } else {
+            SubredditPostIngestor.RunSubredditIngestorOld(subreddit, continuePastUnique);
+        }
     }
 
  }
--- reddit_label/src/main/java/com/reddit/label/SubredditIngestor/SubredditPostIngestor.java
+++ reddit_label/src/main/java/com/reddit/label/SubredditIngestor/SubredditPostIngestor.java
@@ -162,12 +162,6 @@ public class SubredditPostIngestor {
 
     public static void RunSubredditIngestorOld(String subreddit, Boolean stopOnExisting) {
 
-        // Open root webpage.
-        // Extract all of the subreddit posts for that page. 
-        // Ingest unique logic.
-        // Find the next page.
-        // Do it again.
-
         WebDriver driver = new ChromeDriver();
 
         String subredditUrl = String.format("https://old.reddit.com/r/%s/new/", subreddit);
@@ -289,6 +283,136 @@ public class SubredditPostIngestor {
 
         driver.close();
 
+    }
+
+    public static void RunSubredditIngestorOld(String subreddit, Boolean stopOnExisting, String overrideUrl) {
+
+        WebDriver driver = new ChromeDriver();
+
+        String subredditUrl = String.format(
+            "https://old.reddit.com/r/%s/new/%s", 
+            subreddit,
+            overrideUrl
+        );
+
+        driver.get(subredditUrl);
+
+        driver.manage().timeouts().implicitlyWait(Duration.ofMillis(1000));
+
+        Boolean isRunning = true;
+        
+        while (isRunning) {
+
+            try {
+                Thread.sleep(5000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+            List<WebElement> subredditPostElements = driver.findElements(By.className("thing"));
+
+            List<String> existingPostIds = SubredditTablesDB.findAllIds();
+            List<SubredditPost> newPostsToIngest = new ArrayList<SubredditPost>();
+
+            for (WebElement post: subredditPostElements) {
+
+                String relativePostUrl = post.getAttribute("data-permalink");
+
+                if (relativePostUrl == null) {
+                    System.out.println("Null detected when trying to extract relativePostUrl. Skipping...");
+                    continue;
+                }
+
+                String fullPostUrl = "https://www.reddit.com" + relativePostUrl;
+
+                try {
+
+                    // Generating a unique id based on post url:
+                    MessageDigest md = MessageDigest.getInstance("MD5");
+                    byte[] messageDigest = md.digest(fullPostUrl.getBytes());
+
+                    BigInteger no = new BigInteger(1, messageDigest);
+
+                    String idHashText = no.toString(16);
+                    while (idHashText.length() < 32) {
+                        idHashText = "0" + idHashText;
+                    }
+                    
+                    if (existingPostIds.contains(idHashText)) {
+                        System.out.println(
+                            String.format(
+                                "%s (%s) post already in database. Not adding to list of posts to insert", 
+                                idHashText,
+                                fullPostUrl)
+                            );
+                    } else {
+                        System.out.println(
+                            String.format(
+                                "%s (%s) post not found in the database. Adding the list to posts to insert into the db", 
+                                idHashText,
+                                fullPostUrl)
+                            );
+                            
+                            SubredditPost newPost = new SubredditPost(
+                                idHashText, 
+                                subreddit, 
+                                fullPostUrl, 
+                                false
+                            );
+
+                            newPostsToIngest.add(newPost);
+                    }
+
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+
+            if (newPostsToIngest.size() == 0) {
+                System.out.println("No new articles found for individual posts for the pull.");
+
+                if (stopOnExisting) {
+                    System.out.println("stopOnExisting set to true. Exiting program now that postToIngest is empty");
+                    isRunning = false;
+
+                } else {
+                    System.out.println("stopOnExisting set to false, continuing to extract posts even though no uniuqe posts found");
+                }
+            } else {
+                System.out.println(String.format("%d new posts to insert into the database", newPostsToIngest.size()));
+                
+                int numRowsInserted = SubredditTablesDB.InsertBasicSubredditPost(newPostsToIngest);
+                if (numRowsInserted == -1) {
+                    System.err.println("Number of rows inserted into the database is -1. There was some error inserting records into the database");
+                } else {
+                    System.out.println(String.format("%d subreddit posts inserted into the database", numRowsInserted));
+                }
+
+            }
+
+            WebElement nextButton = driver.findElement(By.className("next-button"));
+            String nextUrl = nextButton.findElement(By.tagName("a")).getAttribute("href");
+            
+            if (nextUrl == null) {
+                System.out.println("No next url found - shutting down");
+                return;
+            }
+
+            System.out.println(String.format("%s found as the url to the next page", nextUrl));
+
+            driver.manage().timeouts().implicitlyWait(Duration.ofMillis(2000));
+            try {
+                Thread.sleep(5000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+            driver.get(nextUrl);
+        }
+
+        driver.close();
+
+ 
 
 
     }
