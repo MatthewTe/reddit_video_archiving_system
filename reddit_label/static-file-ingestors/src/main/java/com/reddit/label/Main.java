52da6b057205c5c488871719765fa7ebdb33e8cd
Matthew Teelucksingh <matthewteelucksingh@Matthews-MacBook-Air.local>
2024-06-25 23:06:11 -0700

    Added main hosted:video ingestion Main.java logic

--- reddit_label/static-file-ingestors/src/main/java/com/reddit/label/Main.java
+++ reddit_label/static-file-ingestors/src/main/java/com/reddit/label/Main.java
@@ -10,6 +10,12 @@ import java.sql.Connection;
 import java.sql.SQLException;
 import java.util.List;
 
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.DefaultParser;
+import org.apache.commons.cli.Options;
+import org.apache.commons.cli.ParseException;
+
 import com.reddit.label.BlobStorage.BlobStorageConfig;
 import com.reddit.label.Databases.DB;
 import com.reddit.label.Databases.SubredditTablesDB;
@@ -29,7 +35,7 @@ import io.minio.errors.XmlParserException;
 
 public class Main 
 {
-    public static void main( String[] args ) throws SQLException, InvalidKeyException, ErrorResponseException, InsufficientDataException, InternalException, InvalidResponseException, NoSuchAlgorithmException, ServerException, XmlParserException, IllegalArgumentException, IOException
+    public static void main( String[] args ) throws SQLException, InvalidKeyException, ErrorResponseException, InsufficientDataException, InternalException, InvalidResponseException, NoSuchAlgorithmException, ServerException, XmlParserException, IllegalArgumentException, IOException, InterruptedException
     {
 
 
@@ -41,86 +47,120 @@ public class Main
          *  - Run the static file ingestion. 
          *  - Just keep doing this.
          */
+
+        Options options = new Options();
+        options.addOption("r", "runs", true, "The total number of runs to run the ingestor for");
+        options.addOption("b", "batch_size", true, "The number of reddit posts to run through each batch");
+
+        int numRuns;
+        int batchSize;
+        
+        CommandLineParser parser = new DefaultParser();
+        try {
+
+            CommandLine cmd = parser.parse(options, args);
+            numRuns = Integer.parseInt(cmd.getOptionValue("runs"));
+            batchSize = Integer.parseInt(cmd.getOptionValue("batch_size"));
+
+        } catch (ParseException e) {
+            System.err.println("Error with parsing command line" + e.getMessage());
+            System.exit(1);
+            return;
+        }
+
+
         Connection conn = DB.connect();
-
-        List<SubredditPost> postsToProcess = SubredditTablesDB.getPostsNoStatic(conn, 1);
-        System.out.printf("Queried %d posts ready to ingest static \n", postsToProcess.size());
-
         MinioClient minioClient = MinioClient.builder()
             .endpoint(BlobStorageConfig.getMinioTestEndpoint())
             .credentials(BlobStorageConfig.getMinioTestUserId(), BlobStorageConfig.getMinioTestAccesskey())
             .build();
 
-        for (SubredditPost post: postsToProcess) {
-            
-            System.out.println(post.getId());
 
-            String jsonFileName;
-            if (post.getJsonPostPath() == null) {
-                System.out.printf("%s post has no json file. Downloading json file. \n", post.getId());
-                jsonFileName = SubredditStaticContentIngestor.IngestJSONContent(conn, minioClient, post);
-                System.out.printf("json file for %s post has been ingested as %s \n", post.getId(), jsonFileName);
-            } else {
-                jsonFileName = post.getJsonPostPath();
-            }
+        while (numRuns > 0) {
+            List<SubredditPost> postsToProcess = SubredditTablesDB.getPostsNoStatic(conn, batchSize);
+            System.out.printf("Queried %d posts ready to ingest static \n", postsToProcess.size());
 
-            if (post.getScreenshotPath() == null) {
-                System.out.printf("%s post has no screenshot. Taking screenshot\n", post.getId());
-                String screenshotPath = SubredditStaticContentIngestor.IngestSnapshotImage(conn, minioClient, post);
-                System.out.printf("Screenshot for %s post has been ingessted at %s \n", post.getId(), screenshotPath);
-            }
-
-
-            System.out.printf("Getting json post from database from post %s \n", post.getId());
-            try (InputStream stream = minioClient.getObject(
-                GetObjectArgs.builder()
-                .bucket("reddit-posts")
-                .object(jsonFileName)
-                .build())) {
-
-                // Reading the input Stream:
-                StringBuilder content = new StringBuilder();
-                try (BufferedReader reader = new BufferedReader(new InputStreamReader(stream))) {
-                    String line;
-                    while((line = reader.readLine()) != null) {
-                        content.append(line).append("\n");
-                    }
-                }
-
-                System.out.printf("Beginning Json Parsing \n");
-                RedditJsonParserResponse parsedJsonResponse = RedditJsonParser.parseDefaultRedditPostJson(content.toString());
+            for (SubredditPost post: postsToProcess) {
                 
-                if (parsedJsonResponse.getInitalPostFields().getStaticFileType() == RedditContentPostType.HOSTED_VIDEO) {
+                Thread.sleep(5000);
 
-                    System.out.printf("Post %s has a static file type of video:hosted. Beginning static file ingestion.", post.getId());
+                System.out.println(post.getId());
 
-                    RedditHostedVideoIngestor hostedVideoIngestor = new RedditHostedVideoIngestor();
-                    hostedVideoIngestor.fileToBlob(
-                        post, 
-                        parsedJsonResponse.getInitalPostFields(),
-                        parsedJsonResponse.getFullPostJson(),
-                        conn,
-                        minioClient);
-
-                        System.out.printf("Finished ingesting all data for post %s \n", post.getId());
-
+                String jsonFileName;
+                if (post.getJsonPostPath() == null) {
+                    System.out.printf("%s post has no json file. Downloading json file. \n", post.getId());
+                    jsonFileName = SubredditStaticContentIngestor.IngestJSONContent(conn, minioClient, post);
+                    System.out.printf("json file for %s post has been ingested as %s \n", post.getId(), jsonFileName);
                 } else {
-                    // Update here as I add parsers:
-                    System.out.printf(
-                        "Parsed JSON from post %s. The static file type was %s which is not currently supported. Setting static_file_type to unknown", 
-                        parsedJsonResponse.initalPostFields.getStaticFileType()
-                    );
+                    jsonFileName = post.getJsonPostPath();
+                }
 
-                    int updatedStaticFileTypeResponse = SubredditTablesDB.updateStaticFileType(conn, post.getId(), "unknown");
-                    if (updatedStaticFileTypeResponse != 1) {
-                        System.out.printf("Error in updating static file type. Result integer returned is: %d \n", updatedStaticFileTypeResponse);
+                if (post.getScreenshotPath() == null) {
+                    System.out.printf("%s post has no screenshot. Taking screenshot\n", post.getId());
+                    String screenshotPath = SubredditStaticContentIngestor.IngestSnapshotImage(conn, minioClient, post);
+                    System.out.printf("Screenshot for %s post has been ingessted at %s \n", post.getId(), screenshotPath);
+                }
+
+
+                System.out.printf("Getting json post from database from post %s \n", post.getId());
+                try (InputStream stream = minioClient.getObject(
+                    GetObjectArgs.builder()
+                    .bucket("reddit-posts")
+                    .object(jsonFileName)
+                    .build())) {
+
+                    // Reading the input Stream:
+                    StringBuilder content = new StringBuilder();
+                    try (BufferedReader reader = new BufferedReader(new InputStreamReader(stream))) {
+                        String line;
+                        while((line = reader.readLine()) != null) {
+                            content.append(line).append("\n");
+                        }
+                    }
+
+                    System.out.printf("Beginning Json Parsing \n");
+
+                    Thread.sleep(500);
+                    RedditJsonParserResponse parsedJsonResponse = RedditJsonParser.parseDefaultRedditPostJson(content.toString());
+                    
+                    if (parsedJsonResponse.getInitalPostFields().getStaticFileType() == RedditContentPostType.HOSTED_VIDEO) {
+
+                        System.out.printf("Post %s has a static file type of video:hosted. Beginning static file ingestion.", post.getId());
+
+                        RedditHostedVideoIngestor hostedVideoIngestor = new RedditHostedVideoIngestor();
+                        hostedVideoIngestor.fileToBlob(
+                            post, 
+                            parsedJsonResponse.getInitalPostFields(),
+                            parsedJsonResponse.getFullPostJson(),
+                            conn,
+                            minioClient);
+
+                            System.out.printf("Finished ingesting all data for post %s \n", post.getId());
+
                     } else {
-                        System.out.printf("Sucessfully set the static file type for post %s to unknown", post.getId());
+                        // Update here as I add parsers:
+                        System.out.printf(
+                            "Parsed JSON from post %s. The static file type was %s which is not currently supported. Setting static_file_type to unknown", 
+                            parsedJsonResponse.initalPostFields.getStaticFileType()
+                        );
+
+                        int updatedStaticFileTypeResponse = SubredditTablesDB.updateStaticFileType(conn, post.getId(), "unknown");
+                        if (updatedStaticFileTypeResponse != 1) {
+                            System.out.printf("Error in updating static file type. Result integer returned is: %d \n", updatedStaticFileTypeResponse);
+                        } else {
+                            System.out.printf("Sucessfully set the static file type for post %s to unknown", post.getId());
+                        }
                     }
                 }
+
             }
 
+            numRuns--;
+
+            Thread.sleep(2000);
         }
+
+
 
     }
 
