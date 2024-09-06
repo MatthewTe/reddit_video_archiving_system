dafaf1a4b1f4bb7ca8e8efb99adca9e1d337c232
Matthew Teelucksingh <teelucksingh.matthew1@gmail.com>
2024-09-05 22:21:05 -0700

    Added untested H3 and Date node ingestion DAL

--- graph_ingestor/data_layer/config/core_utils.go
+++ graph_ingestor/data_layer/config/core_utils.go
@@ -2,6 +2,7 @@ package config
 
 import (
 	"context"
+	"time"
 
 	"github.com/neo4j/neo4j-go-driver/v5/neo4j"
 )
@@ -66,4 +67,52 @@ func InsertSpatialIndexNodes(h3Cells []H3Cell, env Neo4JEnvironment, ctx context
 	return createdCellsResult.([]H3Cell), nil
 }
 
-func InsertDatetimeNode() {}
+func InsertDatetimeNodes(Days []time.Time, env Neo4JEnvironment, ctx context.Context) ([]time.Time, error) {
+
+	driver, err := neo4j.NewDriverWithContext(
+		env.URI,
+		neo4j.BasicAuth(env.User, env.Password, ""),
+	)
+	if err != nil {
+		panic(err)
+	}
+	defer driver.Close(ctx)
+
+	err = driver.VerifyConnectivity(ctx)
+	if err != nil {
+		panic(err)
+	}
+
+	session := driver.NewSession(ctx, neo4j.SessionConfig{DatabaseName: "neo4j"})
+	createdDateNodes, err := session.ExecuteWrite(ctx,
+		func(tx neo4j.ManagedTransaction) (any, error) {
+
+			var createdDateNodes []time.Time
+			for _, day := range Days {
+				dayNodeCreationResult, err := tx.Run(
+					ctx,
+					"MERGE (date_node:Date {day: date($date)}) RETURN date_node.day AS date",
+					map[string]any{"date": day.Format("2006-01-02")},
+				)
+				if err != nil {
+					return nil, err
+				}
+
+				dayNodeCreationResponse, err := dayNodeCreationResult.Single(ctx)
+				if err != nil {
+					return nil, err
+				}
+				createdDateNodes = append(createdDateNodes, dayNodeCreationResponse.AsMap()["date"].(time.Time))
+			}
+
+			return createdDateNodes, nil
+		})
+
+	if err != nil {
+		var emptyCreatedDateNodes []time.Time
+		return emptyCreatedDateNodes, err
+	}
+
+	return createdDateNodes.([]time.Time), nil
+
+}
