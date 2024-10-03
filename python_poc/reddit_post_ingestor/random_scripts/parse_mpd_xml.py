9f99591959808425bd71fae7faed6a4e15fc930c
Matthew Teelucksingh <teelucksingh.matthew1@gmail.com>
2024-10-03 14:45:11 -0700

    Working random script that downloads audio and video from MPD file

--- python_poc/reddit_post_ingestor/random_scripts/parse_mpd_xml.py
+++ python_poc/reddit_post_ingestor/random_scripts/parse_mpd_xml.py
@@ -2,13 +2,34 @@ import xml.etree.ElementTree as ET
 from loguru import logger
 import requests 
 import io
+import typing
+
+
+class VideoMPDResult(typing.TypedDict):
+    url: str
+    video_byte_stream: io.BytesIO
+    
+class AudioMPDResult(typing.TypedDict):
+    url: str
+    audio_byte_stream: io.BytesIO
+
+class ParsedMPDResult(typing.TypedDict):
+    mpd_file: str
+    videos_periods: dict[int, VideoMPDResult]
+    audio_periods: dict[int, AudioMPDResult]
 
 # ET.fromstring(country_data_as_string)
-def parse_video_from_mpd_document(mpd_document_str, reddit_post_json_data):
+def parse_video_from_mpd_document(mpd_document_str, reddit_post_json_data) -> ParsedMPDResult:
 
     root = ET.fromstring(mpd_document_str)
     
     namespace = root.tag.split('}')[0].strip('{')
+
+    parsed_result: ParsedMPDResult = {
+        "mpd_file": mpd_document_str,
+        "audio_periods": {},
+        "videos_periods": {}
+    }
 
     for period in root.findall("Period", namespaces={"":namespace}):
         logger.info(f"Period {period.attrib['id']} with a duration of: {period.attrib['duration']}")
@@ -37,17 +58,17 @@ def parse_video_from_mpd_document(mpd_document_str, reddit_post_json_data):
                         logger.info("Extracted video from largest bandwidth representation set")
 
                         reddit_post_base_url = reddit_post_json_data.get("url", None)
-                        video_root_url = representation.find("BaseURL", namespaces={'':namespace})
+                        video_root_url = representation.find("BaseURL", namespaces={'':namespace}).text
 
                         if reddit_post_base_url is not None or video_root_url is not None:
                             video_url = f"{reddit_post_base_url}/{video_root_url}"
                             logger.info(f"Making request to get video from {video_url}")
-                            video_response = requests.get(video_root_url)
+                            video_response = requests.get(video_url)
                             video_response.raise_for_status()
 
                             video_stream = io.BytesIO(initial_bytes=video_response.content)
                             logger.info(f"Extracted video file from reddit with {len(video_response.content)} bytes")
-
+                            parsed_result['videos_periods'][int(period.attrib['id'])] = video_stream
 
             if adaptation_set.attrib['contentType'] =='audio':
                 
@@ -65,10 +86,9 @@ def parse_video_from_mpd_document(mpd_document_str, reddit_post_json_data):
                 for representation in adaptation_set.findall("Representation", namespaces={"":namespace}):
                     if int(representation.attrib['id']) == adaptation_set_audio_bandwith_dict[largest_audio_bandwidth]:
 
-                        # TODO: Implement that actual audio extraction:
                         logger.info("Extracted audio from largest bandwidth representation set")
                         reddit_post_base_url = reddit_post_json_data.get("url", None)
-                        audio_root_url = representation.find("BaseURL", namespaces={"":namespace})
+                        audio_root_url = representation.find("BaseURL", namespaces={"":namespace}).text
 
                         if reddit_post_base_url is not None or audio_root_url is not None:
                             audio_full_url = f"{reddit_post_base_url}/{audio_root_url}"
@@ -78,12 +98,16 @@ def parse_video_from_mpd_document(mpd_document_str, reddit_post_json_data):
                             
                             audio_stream = io.BytesIO(initial_bytes=audio_response.content)
                             logger.info(f"Extracted audio file from reddit with {len(audio_response.content)} bytes")
+                            parsed_result['audio_periods'][int(period.attrib['id'])] = audio_stream
 
+    return parsed_result
 
 if __name__ == "__main__":
 
     with open("/Users/matthewteelucksingh/Repos/java_webpage_content_extractor_POC/python_poc/reddit_post_ingestor/example_data/test.mpd", "r") as f:
         mpd_str = f.read()
 
-        # TODO: Add a dict respponse type to store arrays of potential video/audio byte streams from multiple periods
-        parse_video_from_mpd_document(mpd_document_str=mpd_str)
+        result: ParsedMPDResult = parse_video_from_mpd_document(mpd_document_str=mpd_str, reddit_post_json_data={"url":"https://v.redd.it/65us7xy0hesd1"})
+
+        print(result['videos_periods'][0].read())
+
