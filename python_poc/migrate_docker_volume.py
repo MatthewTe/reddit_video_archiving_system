ad41825ae3cdaf29341283ff322bd2a80d26cbee
Matthew Teelucksingh <teelucksingh.matthew1@gmail.com>
2024-10-06 04:01:17 -0700

    Generated script that performs a backup on a docker volume

---
+++ python_poc/migrate_docker_volume.py
@@ -0,0 +1,42 @@
+import argparse
+import subprocess
+import json
+import sys
+import pprint
+
+from loguru import logger
+from datetime import datetime
+
+# Generates the docker command to generate the backup temp file on the root server
+
+parser = argparse.ArgumentParser()
+parser.add_argument("machine_user", help="The user that will be used to ssh'd into the external machine")
+parser.add_argument("machine_host", help="The ip address or url of the machine that will be ssh'ed into")
+parser.add_argument("volume_name", help="The name/id of the docker volume to be coppied")
+parser.add_argument("backup_path", help="The full path accessable to the local machine where the data will be backed up")
+
+args = parser.parse_args()
+
+docker_volume_info_output = subprocess.Popen(f"ssh {args.machine_user}@{args.machine_host} docker inspect {args.volume_name}", shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE).communicate()
+
+logger.info(f"Result from ssh docker volume inspect: \n {docker_volume_info_output}")
+
+docker_vol_info = json.loads(docker_volume_info_output[0].decode("utf-8"))
+
+logger.info("Decoded docker volume info json: ")
+pprint.pprint(docker_vol_info[0])
+
+logger.info(f"Trying to back up volume: {docker_vol_info[0]['Mountpoint']}")
+
+# Actual data transfer:
+backup_name = f"{datetime.today().strftime('%Y-%m-%d')}_{docker_vol_info[0]['Name']}"
+
+docker_backup_cmd = f'''
+docker run --rm \
+  -v {docker_vol_info[0]['Name']}:/volume \
+  -v {args.backup_path}:/backup \
+  busybox \
+  tar cvf /backup/{backup_name}.tar /volume
+'''
+
+print(docker_backup_cmd)
--- python_poc/reddit_post_ingestor/config.py
+++ python_poc/reddit_post_ingestor/config.py
@@ -20,9 +20,9 @@ def get_secrets(env_path) -> Secrets:
         "minio_url": os.environ.get("MINIO_URL"),
         "minio_access_key": os.environ.get("MINIO_ACCESS_KEY"),
         "minio_secret_key": os.environ.get("MINIO_SECRET_KEY"),
-        "neo4j_url": os.environ.get("NEO4J_URL"),
+        "neo4j_url": os.environ.get("API_NEO4J_URL"),
         "reddit_username": os.environ.get("REDDIT_USERNAME"),
         "reddit_password": os.environ.get("REDDIT_PASSWORD"),
-        "neo4j_read_url": os.environ.get("NEO4J_READ_URL"),
+        "neo4j_read_url": os.environ.get("GRAPH_READ_URL"),
         "neo4j_auth": os.environ.get("NEO4J_AUTH")
     }
