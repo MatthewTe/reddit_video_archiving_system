633b38ccf9152e4bb6e4fbb02de23acee4cb0cf6
Matthew Teelucksingh <teelucksingh.matthew1@gmail.com>
2024-12-30 00:39:20 -0400

    Added CORS support for local development and testing

--- graph_ingestor/rest_api/middleware.go
+++ graph_ingestor/rest_api/middleware.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"os"
 	"time"
 )
 
@@ -14,9 +16,18 @@ type LoggerMiddleware struct {
 }
 
 func (l *LoggerMiddleware) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+
+	if os.Getenv("SERVER_ENV") == "dev" {
+		fmt.Printf("Server dev set to %s. Adding CORS enabling to the middleware\n", os.Getenv("SERVER_ENV"))
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
+		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
+	}
+
 	start := time.Now()
 	l.Handler.ServeHTTP(w, r)
 	log.Printf("%s %s: Time taken - %v", r.Method, r.URL.Path, time.Since(start))
+
 }
 
 // The Constructor
